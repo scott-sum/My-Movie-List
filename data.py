df227525e7df3dd1dccf7f0e612e64d9417b8c3c
Scott Sum <scottsum@outlook.com>
2021-03-07 11:09:39 -0500

    add data script

---
+++ data.py
@@ -0,0 +1,23 @@
+import csv
+from search.models import Movie
+import django
+django.setup()
+
+with open('dataset.csv', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    next(reader, None)
+    for row in reader:
+        _, created = Movie.objects.get_or_create(
+        imdb_id=row[2],
+        title=row[3],
+        year=row[4],
+        genre=row[5],
+        duration=row[6],
+        country=row[7],
+        director=row[9],
+        production_company=row[10],
+        actors=row[11],
+        description=row[12],
+        imdb_url=row[13],
+        poster_url=row[14]
+        recommendations=row[15])
